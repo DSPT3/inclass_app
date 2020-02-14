ff7d552addf81576202c1ce31b264d4200955dc6
kryssyco <kjshepard70@hotmail.com>
2020-02-13 23:02:27 -0700

    Added 'about' route.

--- web_app/app.py
+++ web_app/app.py
@@ -3,6 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!!!! Welcome to Flask!"
-    
+
+@app.route("/about")
+def about():
+    return "About Me!"
+
+
