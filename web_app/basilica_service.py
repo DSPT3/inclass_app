870dd05ee74c6fddc0f400d3026b4c69c333bccf
kryssyco <kjshepard70@hotmail.com>
2020-02-16 22:50:28 -0700

    Making sense of the basilica code.

--- web_app/basilica_service.py
+++ web_app/basilica_service.py
@@ -11,6 +11,13 @@ sentences = [
     "This is a similar sentence!",
     "I don't think this sentence is very similar at all...",
 ]
-with basilica.Connection(BASILICA_API_KEY) as c:
-    embeddings = list(c.embed_sentences(sentences))
-print(embeddings)
+
+connection = basilica.Connection(BASILICA_API_KEY)
+
+embeddings = list(connection.embed_sentences(sentences))
+
+for emb in embeddings:
+    print(type(emb))
+    print(emb)
+    print("----------------")
+    
